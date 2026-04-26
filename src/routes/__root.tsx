e6267f05b3f6069462eba97ac8e65cc47f2409fd
Andrew Lisowski <lisowski54@gmail.com>
2026-04-25 23:35:06 -0700

    favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,6 +102,35 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         rel: "stylesheet",
         href: appCss,
       },
+
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon-16x16.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon-32x32.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "48x48",
+        href: "/favicon-48x48.png",
+      },
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "96x96",
+        href: "/favicon-96x96.png",
+      },
+      {
+        rel: "manifest",
+        href: "/manifest.json",
+      },
       import.meta.env.DEV
         ? {
             rel: "stylesheet",
