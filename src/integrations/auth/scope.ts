fc686fa41db99679398b2571c78f79c529e960e3
Andrew Lisowski <lisowski54@gmail.com>
2026-04-27 07:54:52 -0700

    remove email scope

--- src/integrations/auth/scope.ts
+++ src/integrations/auth/scope.ts
@@ -1,7 +1,7 @@
 import { scope as atprotoScope } from '@atcute/oauth-node-client'
 
 export const scope = [
-  atprotoScope.account({ attr: 'email', action: 'read' }),
+  // atprotoScope.account({ attr: 'email', action: 'read' }),
   atprotoScope.blob({ accept: ['image/*', 'video/*'] }),
   atprotoScope.include({ nsid: 'fyi.atstore.authBasic' }),
 ]
